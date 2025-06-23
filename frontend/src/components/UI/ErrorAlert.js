cab890a7c49a1078b6d6df250dbdccec288cfd71
ngocdt <tanngocvnet@gmail.com>
2025-06-23 22:32:36 +0700

    delete ! in file ErrorAlert.js file

--- frontend/src/components/UI/ErrorAlert.js
+++ frontend/src/components/UI/ErrorAlert.js
@@ -5,7 +5,7 @@ import './ErrorAlert.css';
 function ErrorAlert(props) {
   return (
     <section className='error-alert'>
-      <h2>Something went wrong!! To CI/CD</h2>
+      <h2>Something went wrong! To CI/CD</h2>
       <p>{props.errorText}</p>
     </section>
   );
