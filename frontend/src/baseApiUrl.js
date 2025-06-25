f9c0943a068b568dea6adca925e2f7d671deb479
ngocdt <tanngocvnet@gmail.com>
2025-06-25 16:09:56 +0700

    comment line 'branches' and edit 'baseApiUrl.js'

--- frontend/src/baseApiUrl.js
+++ frontend/src/baseApiUrl.js
@@ -1,3 +1,3 @@
 // export const API_URL = "http://localhost:8080";
 // export const API_URL = process.env.BACKEND_API_BASE_URL || "http://localhost:8080";
-export const API_URL = "http://appreact-test-lb-1090043962.ap-southeast-1.elb.amazonaws.com" || "http://localhost:8080";
+export const API_URL = "http://172.31.43.111:8080" || "http://localhost:8080";
